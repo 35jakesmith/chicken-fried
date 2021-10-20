ae4a940bdd2195d50adcbb70f9f2187e7df2345f
35jakesmith <35jakesmith@gmail.com>
2021-10-19 18:07:13 -0600

    add js and classes

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,37 @@
-moment().format('MMMM Do YYYY, h:mm:ss a');
+// Current date at top of page using moment.js
+$(document).ready(function () {
+    var currentDate = moment().format('dddd, MMMM Do YYYY');
+    $("#currentDay").text(currentDate);
+
+    
+    // Get current hour.
+    var currentHour = moment().hour();
+
+    // Updating colors in time blocks using jquery
+    var updateBlock = function() {
+        $(".time-block").each(function () {
+
+            var timeBlockHourTime = parseInt($(this).attr("id").split("-")[1]);
+            console.log("This is the time block hour: " + timeBlockHourTime);
+
+            // If time period is before current time add class 'past'
+            // If time period is at current time add class 'present'
+            // If time period is  in the future add class 'future'
+            if (timeBlockHourTime < currentHour) {
+                $(this).addClass("past");
+            } else if (timeBlockHourTime === currentHour) {
+                $(this).removeClass("past");
+                $(this).addClass("present");
+            } else {
+                $(this).removeClass("past");
+                $(this).removeClass("present");
+                $(this).addClass("future");
+            }
+        });
+    };
+
+    updateBlock();
+    var timeBlockUpdated = setInterval(updateBlock, 30000);
+});
+
+
